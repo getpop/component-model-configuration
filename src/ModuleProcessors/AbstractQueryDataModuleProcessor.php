3d06faec88d8212abed043139784b47b2bd8d9b7
Leo <losoviz@gmail.com>
2019-10-12 16:39:05 +0800

    Moved ModuleProcessor classes to ComponentModel

---
+++ src/ModuleProcessors/AbstractQueryDataModuleProcessor.php
@@ -0,0 +1,8 @@
+<?php
+namespace PoP\ConfigurationComponentModel\ModuleProcessors;
+use PoP\ComponentModel\ModuleProcessors\QueryDataModuleProcessorTrait;
+
+abstract class AbstractQueryDataModuleProcessor extends AbstractModuleProcessor
+{
+    use QueryDataModuleProcessorTrait;
+}
