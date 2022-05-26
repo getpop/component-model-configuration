879713b9ab392f5606ad4a7cb9daf22cf0d7b1b6
leoloso <leo@getpop.org>
2022-05-26 14:38:45 +0000

    Merge pull request #1602 from leoloso/Rename-FilterInputProcessor-to-FilterInput
    Renamed FilterInputProcessor to FilterInput

---
+++ src/ComponentProcessors/AbstractQueryDataComponentProcessor.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ConfigurationComponentModel\ComponentProcessors;
+
+use PoP\ComponentModel\ComponentProcessors\QueryDataComponentProcessorInterface;
+use PoP\ComponentModel\ComponentProcessors\QueryDataComponentProcessorTrait;
+use PoP\ComponentModel\QueryInputOutputHandlers\ActionExecutionQueryInputOutputHandler;
+
+abstract class AbstractQueryDataComponentProcessor extends AbstractComponentProcessor implements QueryDataComponentProcessorInterface
+{
+    use QueryDataComponentProcessorTrait;
+
+    private ?ActionExecutionQueryInputOutputHandler $actionExecutionQueryInputOutputHandler = null;
+
+    final public function setActionExecutionQueryInputOutputHandler(ActionExecutionQueryInputOutputHandler $actionExecutionQueryInputOutputHandler): void
+    {
+        $this->actionExecutionQueryInputOutputHandler = $actionExecutionQueryInputOutputHandler;
+    }
+    final protected function getActionExecutionQueryInputOutputHandler(): ActionExecutionQueryInputOutputHandler
+    {
+        return $this->actionExecutionQueryInputOutputHandler ??= $this->instanceManager->getInstance(ActionExecutionQueryInputOutputHandler::class);
+    }
+}
